33fd74cd8eb3aa7798e39cb5619ca74e6bc73167
Hoon <hyo04044@naver.com>
2023-11-04 22:21:24 +0900

    AG-55 [BE] Fix : User Entity

--- server/src/main/java/artgarden/server/domain/User.java
+++ server/src/main/java/artgarden/server/domain/User.java
@@ -1,2 +1,32 @@
-package artgarden.server.domain;public class User {
+package artgarden.server.domain;
+
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long user_id;
+
+    private String user_password;
+
+    @Column(nullable = false)
+    private String user_name;
+
+    private Date user_birth;
+
+    @Column(unique = true, nullable = false)
+    private String user_email;
+
+    private String user_gender;
+
+    @Column(unique = true)
+    private String user_phone;
+
+    private Date user_created_at;
+
+
 }
