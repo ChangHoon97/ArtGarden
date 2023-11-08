658f9ec8b93bc418793bb61d8d022bff88ecf40c
FordangIT <fordang0819@gmail.com>
2023-11-08 21:19:16 +0900

    AG-57 [FE]feat: nav style 진행중

--- client/src/app/components/NavMenu.tsx
+++ client/src/app/components/NavMenu.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
 
-const ACTIVE_ROUTE = "py-1 px-2 text-grey-300 bg-gray-700";
+const ACTIVE_ROUTE = "py-1 px-2 text-white";
 const INACTIVE_ROUTE =
-  "py-1 px-2 text-gray-500 hover:text-gray-300 hover:bg-gray-700";
+  "py-1 px-2 text-white hover:text-white hover:bg-gray-700";
 function AuthButton() {
   const { data: session } = useSession();
 
@@ -13,14 +13,24 @@ function AuthButton() {
     return (
       <>
         {session?.user?.name} <br />
-        <button onClick={() => signOut()}>Sign out</button>
+        <button
+          className="w-36 h-12 text-mainyellow rounded-2xl border-2 border-mainyellow bg-mainblack"
+          onClick={() => signOut()}
+        >
+          Sign out
+        </button>
       </>
     );
   }
   return (
     <>
-      Not signed in <br />
-      <button onClick={() => signIn()}>Sign in</button>
+      로그인 안됨 <br />
+      <button
+        className="w-36 h-12 text-mainyellow rounded-2xl border-2 border-mainyellow bg-mainblack"
+        onClick={() => signIn()}
+      >
+        Sign in
+      </button>
     </>
   );
 }
@@ -28,52 +38,36 @@ function AuthButton() {
 export default function NavMenu() {
   const pathname = usePathname();
   return (
-    <div>
-      <AuthButton />
-      <hr className="my-4" />
-      <ul>
-        <Link href="/">
-          <li className={pathname === "/" ? ACTIVE_ROUTE : INACTIVE_ROUTE}>
-            Home
-          </li>
-        </Link>
-        <Link href="/protected">
-          <li
-            className={
-              pathname === "/protected" ? ACTIVE_ROUTE : INACTIVE_ROUTE
-            }
-          >
-            Protected page
-          </li>
-        </Link>
-        <Link href="/serverAction">
-          <li
-            className={
-              pathname === "/serveraction" ? ACTIVE_ROUTE : INACTIVE_ROUTE
-            }
-          >
-            Server Action
-          </li>
-        </Link>
-        <Link href="/protected">
-          <li
-            className={
-              pathname === "/protected" ? ACTIVE_ROUTE : INACTIVE_ROUTE
-            }
-          >
-            API From Client
-          </li>
-        </Link>
-        <Link href="/protected">
-          <li
-            className={
-              pathname === "/protected" ? ACTIVE_ROUTE : INACTIVE_ROUTE
-            }
-          >
-            API From Server
-          </li>
-        </Link>
-      </ul>
+    <div className="flex  bg-mainred py-16 px-20">
+      <div className="flex flex-row mx-60 px-8 ">
+        <img src="logo.jpg" alt="ArtGarden 로고 이미지" className="w-44 h-8" />
+        <ul className="flex space-x-4 ">
+          <Link href="/">
+            <li className={pathname === "/" ? ACTIVE_ROUTE : INACTIVE_ROUTE}>
+              Home
+            </li>
+          </Link>
+          <Link href="/protected">
+            <li
+              className={
+                pathname === "/protected" ? ACTIVE_ROUTE : INACTIVE_ROUTE
+              }
+            >
+              Protected page
+            </li>
+          </Link>
+          <Link href="/serverAction">
+            <li
+              className={
+                pathname === "/serveraction" ? ACTIVE_ROUTE : INACTIVE_ROUTE
+              }
+            >
+              Server Action
+            </li>
+          </Link>
+        </ul>
+        <AuthButton />
+      </div>
     </div>
   );
 }
