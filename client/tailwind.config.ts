ba4daf1e0e74ca92d5b234bd6327a44874c18d79
FordangIT <fordang0819@gmail.com>
2023-11-10 21:53:56 +0900

    AG-59 [FE]feat: 버튼 재사용 진행중

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,20 +1,42 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      mainred: "#d73d55",
+      mainyellow: "#fff200",
+      mainblack: "#101824",
+      mainnavy: "#090e24",
+      mainbrown: "#511908",
+      mainwhite: "#ffffff",
+    },
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
-  plugins: [],
-}
-export default config
+  plugins: [
+    plugin(function ({ addComponents }) {
+      addComponents({
+        ".btn-yellow": {
+          textColors: "#fff200",
+          fontWeight: "300",
+          borderRadius: ".25rem",
+          borderWidth: "200",
+          borderColor: "#fff200",
+          backgroundColor: "#090e24",
+        },
+      });
+    }),
+  ],
+};
+export default config;
