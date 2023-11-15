5032c2c4d5adde805b3ecd5e2e21a8be5a4d3feb
ChangHoon <hyo04044@naver.com>
2023-11-15 22:41:12 +0900

    AG-28 [BE] Fix : Entity name change(Rank -> WeeklyRank)

--- server/src/main/java/artgarden/server/service/RankService.java
+++ server/src/main/java/artgarden/server/service/RankService.java
@@ -1,13 +1,12 @@
 package artgarden.server.service;
 
-import artgarden.server.entity.Rank;
+import artgarden.server.entity.WeeklyRank;
 import artgarden.server.repository.RankRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -15,7 +14,7 @@ import java.util.List;
 public class RankService {
     private final RankRepository rankRepository;
 
-    public Rank findByRankDate(LocalDate rankDate){
+    public WeeklyRank findByRankDate(LocalDate rankDate){
         return rankRepository.findByRankDate(rankDate);
     }
 }
