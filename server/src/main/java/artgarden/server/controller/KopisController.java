f09ee8f2840881429cf7d7379cd4f53cb2feca59
ChangHoon <hyo04044@naver.com>
2023-11-15 22:34:37 +0900

    AG-28 [BE] Feat : add Swagger

--- server/src/main/java/artgarden/server/controller/KopisController.java
+++ server/src/main/java/artgarden/server/controller/KopisController.java
@@ -1,6 +1,8 @@
 package artgarden.server.controller;
 
 import artgarden.server.service.KopisService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -15,12 +17,13 @@ import java.time.format.DateTimeFormatter;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
-@Tag(name = "KopisApi", description = "Kopis OpenApi를 활용한 API")
+@Tag(name = "KopisApi", description = "Kopis OpenApi를 활용하여 수동으로 DB에 정보 저장, 매일 오전 10시 스케쥴링으로 자동 저장됨")
 public class KopisController {
 
     private final KopisService kopisService;
 
-    //초기 DB에 전체 데이터 저장(06/01~11/09)
+    @Operation(summary = "이후 한달간 공연 예정 상태의 공연 저장", description = "/manual/update/upcoming")
+    @ApiResponse()
     @GetMapping("/manual/update/upcoming")
     public ResponseEntity<String> manualUpcomingUpdate(){
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
@@ -31,21 +34,23 @@ public class KopisController {
         return ResponseEntity.ok("Data save successfully");
     }
 
+    @Operation(summary = "현재 공연중 상태인 공연 저장", description = "/manual/update/ongoing")
     @GetMapping("/manual/update/ongoing")
     public ResponseEntity<String> manualOngoingUpdate(){
 
         kopisService.updateOngoing();
 
-        return ResponseEntity.ok("Data update successfully");
+        return ResponseEntity.ok("Data save successfully");
     }
 
+    @Operation(summary = "해당날짜로 까지 1주일간의 공연 순위 저장", description = "/manual/update/rank/20231114")
     @GetMapping("/manual/update/rank/{rankDate}")
     public ResponseEntity<String> manualRankUpdate(@PathVariable String rankDate){
 //        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd");
 //        String standardDate = LocalDate.now().minusDays(1).format(formatter);
         kopisService.updateRank("week",rankDate);
 
-        return ResponseEntity.ok("Data update successfully");
+        return ResponseEntity.ok("Data save successfully");
     }
 
 
