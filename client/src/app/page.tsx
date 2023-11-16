5f582944d517fc022fc15090de1f9208a3525c1f
FordangIT <fordang0819@gmail.com>
2023-11-16 21:01:26 +0900

    AG-49[FE]feat: 메인페이지에 컴포넌트들 추가

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,19 @@
 import { use } from "react";
 import MainVision from "./components/MainVision";
+import TalkVision from "./components/TalkVision";
+import PerformIntro from "./components/perform/PerformIntro";
+import PerformInfo from "./components/perform/PerformInfo";
+import PerformRank from "./components/perform/PerfromRank";
+import ReviewMain from "./components/review/ReviewMain";
 export default function Home() {
   return (
     <>
       <MainVision />
+      <TalkVision />
+      <PerformIntro />
+      <PerformInfo />
+      <PerformRank />
+      <ReviewMain />
     </>
   );
 }
