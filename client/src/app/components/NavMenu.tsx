7698352e31ba1c3221f6a6ccc4bb97249c3e503a
FordangIT <fordang0819@gmail.com>
2023-11-16 20:59:40 +0900

    AG-57[FE]feat: nav css 수정

--- client/src/app/components/NavMenu.tsx
+++ client/src/app/components/NavMenu.tsx
@@ -2,36 +2,34 @@
 import Link from "next/link";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { usePathname } from "next/navigation";
+import Button from "./Button";
 
 const ACTIVE_ROUTE =
-  "py-1 px-2 text-mainwhite hover:text-mainyellow  hover:bg-gray-700 ml-10";
+  "py-1 px-2 text-mainwhite hover:text-mainyellow  hover:bg-gray-700 ml-20";
 const INACTIVE_ROUTE =
-  "py-1 px-2 text-mainwhite hover:text-mainyellow hover:bg-gray-700  ml-10";
+  "py-1 px-2 text-mainwhite hover:text-mainyellow hover:bg-gray-700  ml-20";
 function AuthButton() {
   const { data: session } = useSession();
 
   if (session) {
     return (
       <>
-        {session?.user?.name} <br />
-        <button
-          className="w-36 h-12 text-mainyellow rounded-2xl border-2 border-mainyellow bg-mainblack ml-40"
-          onClick={() => signOut()}
-        >
+        <div className="text-xl font-medium hover:text-mainyellow">
+          {session?.user?.name}님
+        </div>{" "}
+        <br />
+        <Button color="black" size="sm" onClick={() => signOut()}>
           Sign out
-        </button>
+        </Button>
       </>
     );
   }
   return (
     <>
       <br />
-      <button
-        className="w-36 h-12 text-mainyellow rounded-2xl border-2 border-mainyellow bg-mainblack ml-40"
-        onClick={() => signIn()}
-      >
+      <Button color="black" size="sm" onClick={() => signIn()}>
         Sign in
-      </button>
+      </Button>
     </>
   );
 }
@@ -39,10 +37,10 @@ function AuthButton() {
 export default function NavMenu() {
   const pathname = usePathname();
   return (
-    <div className="flex  bg-mainred py-16 px-20">
+    <div className="flex  bg-mainred pt-16 pb-6 px-20">
       <div className="flex flex-row mx-60 px-8 ">
         <img src="logo.jpg" alt="ArtGarden 로고 이미지" className="w-44 h-8" />
-        <ul className="flex space-x-4">
+        <ul className="flex space-x-4 text-base ">
           <Link href="/">
             <li className={pathname === "/" ? ACTIVE_ROUTE : INACTIVE_ROUTE}>
               <p className="text-white">Home</p>
