7d009e8fb9047944ca98c4c46f543b17014fa369
FordangIT <fordang0819@gmail.com>
2023-11-21 21:48:14 +0900

    feat: 메인 페이지 리뷰 생성

--- client/src/app/components/review/ReviewMain.tsx
+++ client/src/app/components/review/ReviewMain.tsx
@@ -1,8 +1,67 @@
 export default function ReviewMain() {
+  const fakeData = [
+    {
+      id: 1,
+      photo: "",
+      content: "Description 1",
+      star: "_____",
+      day: "2023-11-17",
+    },
+    {
+      id: 2,
+      photo: "",
+      content: "Description 2",
+      start: "___",
+      day: "2023-11-17",
+    },
+    {
+      id: 3,
+      photo: "",
+      content: "Description 1",
+      star: "_____",
+      day: "2023-11-17",
+    },
+    {
+      id: 4,
+      photo: "",
+      content: "Description 2",
+      start: "___",
+      day: "2023-11-17",
+    },
+    {
+      id: 5,
+      photo: "",
+      content: "Description 1",
+      star: "_____",
+      day: "2023-11-17",
+    },
+    {
+      id: 6,
+      photo: "",
+      content: "Description 2",
+      start: "___",
+      day: "2023-11-17",
+    },
+  ];
   return (
     <>
-      <div className="bg-mainwhite py-44"></div>
-      <div>리뷰</div>
+      <div className="flex space-x-4">
+        <div className="bg-mainwhite py-44">
+          <div>리뷰</div>
+          {fakeData.map((el) => (
+            <div key={el.id} className="w-72 h-80 rounded-md shadow-xl">
+              <div className="bg-mainrealblack h-1/2 rounded-md">
+                {el.photo}
+              </div>
+              <div className="">
+                <div>{el.content}</div>
+                <div>{el.star}</div>
+                <div>{el.day}</div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
