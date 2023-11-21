bdc3aa1c5d0fd74577fc1af6bfa7a9ffbe0237dd
FordangIT <fordang0819@gmail.com>
2023-11-21 21:47:45 +0900

    feat: 공연 정보 데이터 가져오기 확인

--- client/src/app/components/perform/PerformInfo.tsx
+++ client/src/app/components/perform/PerformInfo.tsx
@@ -1,48 +1,39 @@
+"use client";
+import { Perform_Info } from "@/app/types";
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
+
+async function getPerForm() {
+  return (await fetch(`${process.env.BACKEND_URL}/performances`).then((res) =>
+    res.json()
+  )) as Perform_Info[];
+}
+
 export default function PerformInfo() {
+  const { data } = useQuery<Perform_Info[]>({
+    queryKey: ["perform-info"],
+    queryFn: () => getPerForm(),
+  });
   return (
     <>
       <div className="bg-mainrealblack">
-        <div className="flex">
-          <div className="bg-mainwhite w-1/4 mx-2 py-60">
-            <div>KREIS</div>
-            <div>
-              <div className="mt-15"></div>
-              <div className="">
-                공연 정보글공연 정보글공연 정보글공연 정보글공연 정보글공연
-                정보글
-              </div>
+        <div className="flex flex-wrap justify-center">
+          {data?.map((el) => (
+            <div
+              key={el.id}
+              className="bg-mainwhite w-1/5 mx-2 mt-4 py-60 "
+              // style={{ flex: "1 0 25%" }}
+            >
+              <img src={el.posterUrl}></img>
+              {/* <div>{el.name}</div>
+              <div>
+                <div className="mt-15">{el.endDate}</div>
+                <div className="">{el.price}</div>
+                <div>{el.place}</div>
+              </div> */}
             </div>
-          </div>
-          <div className="bg-mainwhite w-1/4 mx-2 py-60">
-            <div>KREIS</div>
-            <div>
-              <div className="mt-15"></div>
-              <div className="">
-                공연 정보글공연 정보글공연 정보글공연 정보글공연 정보글공연
-                정보글
-              </div>
-            </div>
-          </div>
-          <div className="bg-mainwhite w-1/4 mx-2 py-60">
-            <div>KREIS</div>
-            <div>
-              <div className="mt-15"></div>
-              <div className="">
-                공연 정보글공연 정보글공연 정보글공연 정보글공연 정보글공연
-                정보글
-              </div>
-            </div>
-          </div>
-          <div className="bg-mainwhite w-1/4 mx-2 py-60">
-            <div>KREIS</div>
-            <div>
-              <div className="mt-15"></div>
-              <div className="">
-                공연 정보글공연 정보글공연 정보글공연 정보글공연 정보글공연
-                정보글
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
