3fe890f9fcc7652cdee6912245bff91f31201f78
Hoon <hyo04044@naver.com>
2023-11-27 22:57:03 +0900

    [BE] Fix : WeeklyRank strategy

--- server/src/main/java/artgarden/server/entity/WeeklyRank.java
+++ server/src/main/java/artgarden/server/entity/WeeklyRank.java
@@ -14,7 +14,7 @@ import java.util.List;
 public class WeeklyRank {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @Column(unique = true)
     private LocalDate rankDate;
