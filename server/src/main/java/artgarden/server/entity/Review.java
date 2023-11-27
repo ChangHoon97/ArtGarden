a9def943ef6c203c6e1f289bd1cb69a3d016a23e
ChangHoon <hyo04044@naver.com>
2023-11-27 14:15:33 +0900

    AG-29 [BE] Feat : Review function create

--- server/src/main/java/artgarden/server/entity/Review.java
+++ server/src/main/java/artgarden/server/entity/Review.java
@@ -1,5 +1,7 @@
 package artgarden.server.entity;
 
+import artgarden.server.entity.dto.reviewDto.ReviewDto;
+import artgarden.server.entity.dto.reviewDto.ReviewUpdateDto;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,5 +23,19 @@ public class Review {
     private Long member_id;
     private LocalDateTime created_at;
     private LocalDateTime modified_at;
+
+    public void createFromDto(ReviewDto dto){
+        this.perform_id = dto.getPerform_id();
+        this.content = dto.getContent();
+        this.rate = dto.getRate();
+        this.member_id = dto.getMember_id();
+        this.created_at = dto.getCreated_at();
+    }
+
+    public void updateFromDto(ReviewUpdateDto dto){
+        this.content = dto.getContent();
+        this.rate = dto.getRate();
+        this.modified_at = dto.getModified_at();
+    }
 
 }
