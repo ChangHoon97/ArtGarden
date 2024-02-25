fb38ee689b00f302c17cb2e1947a7884929120a1
Hoon <hyo04044@naver.com>
2024-02-26 02:04:17 +0900

    [be] chores : get Log

---
+++ server/src/main/java/artgarden/server/config/LoggingInterceptor.java
@@ -0,0 +1,28 @@
+package artgarden.server.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import java.time.LocalDateTime;
+
+public class LoggingInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String uri = request.getRequestURI();
+        LocalDateTime currentTime = LocalDateTime.now();
+        System.out.println("Request to " + uri + " at " + currentTime);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        // This method is intentionally left blank
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        // This method is intentionally left blank
+    }
+}
