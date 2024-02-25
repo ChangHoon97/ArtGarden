0fede340d2e970e411bccfa61b2786516dced2c7
Hoon <hyo04044@naver.com>
2024-02-26 01:57:40 +0900

    [be] chores : set timezone

--- server/src/main/java/artgarden/server/ServerApplication.java
+++ server/src/main/java/artgarden/server/ServerApplication.java
@@ -1,8 +1,11 @@
 package artgarden.server;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.TimeZone;
 
 @SpringBootApplication
 @EnableScheduling
@@ -10,6 +13,12 @@ public class ServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		// timezone 설정
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
 	}
 
 }
