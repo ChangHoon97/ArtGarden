55ecbc8c11a47f3fa208214036093a91771fadd4
CHANGHOON <hyo040441@gmail.com>
2024-04-04 00:09:04 +0900

    [BE] Fix : fix healthcheck route

--- server/src/main/java/artgarden/server/controller/AWSController.java
+++ server/src/main/java/artgarden/server/controller/AWSController.java
@@ -1,5 +1,6 @@
 package artgarden.server.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
@@ -9,10 +10,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
-@Tag(name="기본 컨트롤러")
-public class HomeController {
+@Tag(name="AWS 컨트롤러")
+public class AWSController {
 
-    @GetMapping("/")
+    @Operation(summary = "ec2 대상그룹 health check")
+    @GetMapping("/aws/healthcheck")
     public ResponseEntity<String> home(){
         String result = "ok";
         return ResponseEntity.status(HttpStatus.OK).body(result);
