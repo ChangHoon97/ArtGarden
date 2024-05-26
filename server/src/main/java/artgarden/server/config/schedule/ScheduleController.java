5e4c46d6e4e4e1e4212414408a7d01214663aa3b
CHANGHOON <hyo040441@gmail.com>
2024-05-26 22:14:16 +0900

    [BE] Feat : add Culture schedule controller

--- server/src/main/java/artgarden/server/config/schedule/ScheduleController.java
+++ server/src/main/java/artgarden/server/config/schedule/ScheduleController.java
@@ -1,8 +1,8 @@
 package artgarden.server.config.schedule;
 
 import artgarden.server.common.util.UtilBean;
+import artgarden.server.culture.service.CultureService;
 import artgarden.server.kopis.service.KopisService;
-import artgarden.server.kopis.service.KopisServiceImpl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -10,19 +10,19 @@ import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Component
 @RequiredArgsConstructor
 @Slf4j
-public class KopisScheduler {
+public class ScheduleController {
 
     private final KopisService kopisService;
+    private final CultureService cultureService;
 
 
-    @Scheduled(cron = "0 00 10 * * ?")
-    public void dailyUpdate(){
-        log.info("스케쥴링 자동 업데이트 시작 : " + LocalDateTime.now());
+    @Scheduled(cron = "0 00 20 * * ?")
+    public void KopisDailyUpdate(){
+        log.info("KOPIS 스케쥴링 자동 업데이트 시작 : " + LocalDateTime.now());
         kopisService.updateUpcoming( UtilBean.formatDate(LocalDate.now().plusMonths(1)));    //standard = 한달 이후
         kopisService.deletePerformed(LocalDate.now().minusMonths(1));   //standard = 한달 이전
         kopisService.updatePerformStatus();
@@ -30,7 +30,14 @@ public class KopisScheduler {
         kopisService.updateRank("week", UtilBean.formatDate(LocalDate.now().minusDays(1)));
         kopisService.updateAreaCode();
         kopisService.updateGenreCode();
-        log.info("스케쥴링 자동 업데이트 종료 : " + LocalDateTime.now());
+        log.info("KOPIS 스케쥴링 자동 업데이트 종료 : " + LocalDateTime.now());
+    }
+
+    @Scheduled(cron = "0 00 00 * * ?")
+    public void CultureDailyUpdate() throws Exception {
+        log.info("CULTURE 스케쥴링 자동 업데이트 시작 : " + LocalDateTime.now());
+        cultureService.updateExhibitList("D000");
+        log.info("CULTURE 스케쥴링 자동 업데이트 종료 : " + LocalDateTime.now());
     }
 
 }
