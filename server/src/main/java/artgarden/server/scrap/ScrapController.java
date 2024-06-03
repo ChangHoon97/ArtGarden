53575ee2afbcf62e7e133d98853079516d791834
CHANGHOON <hyo040441@gmail.com>
2024-06-03 22:00:51 +0900

    [BE] Feat : myScrapList

--- server/src/main/java/artgarden/server/scrap/ScrapController.java
+++ server/src/main/java/artgarden/server/scrap/ScrapController.java
@@ -7,26 +7,49 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @Tag(name = "Scrap", description = "찜하기")
 public class ScrapController {
     private final ScrapService scrapService;
 
+    //나의 스크랩
+    @GetMapping("/myScraps")
     public ResponseEntity<ScrapPageDTO> getScrapByMember(@Parameter(description = "표시할 페이지")
-                                                       @RequestParam(defaultValue = "1") int page,
+                                                         @RequestParam(defaultValue = "1") int page,
                                                          @Parameter(description = "한 페이지에 볼 게시물 수")
-                                                       @RequestParam(defaultValue = "8") int size,
+                                                         @RequestParam(defaultValue = "8") int size,
                                                          HttpServletRequest request){
         HttpSession session = request.getSession();
         String memberid = (String) session.getAttribute("memberid");
         Pageable pageable = PageRequest.of(page-1, size);
-        scrapService.selectMyScrapList(memberid, pageable);
+        ScrapPageDTO scrap = scrapService.selectMyScrapList(memberid, pageable);
+
+        return ResponseEntity.ok(scrap);
     }
+
+    //스크랩하기
+    @PostMapping("/scraps")
+    public ResponseEntity<String> mergeintoScrap(@Parameter(description = "공연/전시ID")
+                                                 @RequestParam String objectid,
+                                                 HttpServletRequest request){
+        HttpSession session = request.getSession();
+        String memberid = (String) session.getAttribute("memberid");
+
+        String result = "";
+
+        return ResponseEntity.ok(result);
+    }
+
+
 }
---
+++ server/src/main/java/artgarden/server/scrap/entity/dto/ScrapingDTO.java
@@ -0,0 +1,13 @@
+package artgarden.server.scrap.entity.dto;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ScrapingDTO {
+
+    @Schema(description = "공연/전시 ID", example = "EX123456")
+    private String objectid;
+}
