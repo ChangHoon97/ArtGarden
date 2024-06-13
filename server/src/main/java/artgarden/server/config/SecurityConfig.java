5c4d8f59f1cf313eb03b0c0af80e0359a002ab5f
CHANGHOON <hyo040441@gmail.com>
2024-06-14 00:13:28 +0900

    [BE] Test

--- server/src/main/java/artgarden/server/config/SecurityConfig.java
+++ server/src/main/java/artgarden/server/config/SecurityConfig.java
@@ -15,6 +15,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -57,6 +58,7 @@ public class SecurityConfig {
                         config.setAllowCredentials(true);
                         config.setAllowedHeaders(Collections.singletonList("*"));
                         config.setMaxAge(3600L);
+                        config.setAllowedHeaders(List.of("*"));
                         return config;
                     }
                 }))
