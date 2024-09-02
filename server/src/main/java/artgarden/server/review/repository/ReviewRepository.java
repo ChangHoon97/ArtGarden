110a0256bfc6225fd7360d1f89f97dd0ec11b072
CHANGHOON <hyo040441@gmail.com>
2024-09-02 18:46:19 +0900

    [BE] Fix : review member condition

--- server/src/main/java/artgarden/server/review/repository/ReviewRepository.java
+++ server/src/main/java/artgarden/server/review/repository/ReviewRepository.java
@@ -12,7 +12,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @Query("SELECT new artgarden.server.review.entity.dto.ReviewListDTO(r, COALESCE(p.posterurl,e.posterurl), COALESCE(p.name, e.name), COALESCE(p.genre, e.genre), m.nickname) " +
             "FROM Review r " +
-            "LEFT OUTER JOIN Member m on r.memberid = m.loginid " +
+            "LEFT OUTER JOIN Member m on r.memberid = m.loginid AND m.delyn = false " +
             "LEFT OUTER JOIN Performance p on p.id = :object_id " +
             "LEFT OUTER JOIN Exhibit e on e.id = :object_id " +
             "WHERE r.objectid = :object_id " +
@@ -23,7 +23,7 @@ public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     @Query("SELECT new artgarden.server.review.entity.dto.ReviewListDTO(r, COALESCE(p.posterurl,e.posterurl), COALESCE(p.name, e.name), COALESCE(p.genre, e.genre), m.nickname) " +
             "FROM Review r " +
-            "LEFT OUTER JOIN Member m on r.memberid = m.loginid " +
+            "LEFT OUTER JOIN Member m on r.memberid = m.loginid AND m.delyn = false " +
             "LEFT OUTER JOIN Performance p on p.id = r.objectid " +
             "LEFT OUTER JOIN Exhibit e on e.id = r.objectid " +
             "ORDER BY r.regdt DESC")
