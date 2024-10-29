7225e429439d9d6af255f0db9fdc47f80de06d04
CHANGHOON <hyo040441@gmail.com>
2024-10-29 19:56:04 +0900

    [BE] Feat : add schedule log

--- server/src/main/java/artgarden/server/common/ScheduleController.java
+++ server/src/main/java/artgarden/server/common/ScheduleController.java
@@ -26,10 +26,15 @@ public class ScheduleController {
         kopisService.updateUpcoming( UtilBean.formatDate(LocalDate.now().plusMonths(1)));    //standard = 한달 이후
         //kopisService.deletePerformed(LocalDate.now().minusMonths(1));   //standard = 한달 이전
         kopisService.updatePerformStatus();
+        log.info("공연 완료처리 끝");
         kopisService.updateOngoing();
+        log.info("오늘 공연중인 공연 저장 처리 끝");
         kopisService.updateRank("week", UtilBean.formatDate(LocalDate.now().minusDays(1)));
+        log.info("랭크 처리 끝");
         kopisService.updateAreaCode();
+        log.info("지역코드 처리 끝");
         kopisService.updateGenreCode();
+        log.info("장르코드 처리 끝");
         log.info("KOPIS 스케쥴링 자동 업데이트 종료 : " + LocalDateTime.now());
     }
 
