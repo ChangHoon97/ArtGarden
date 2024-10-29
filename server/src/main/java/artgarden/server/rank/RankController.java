17aed0fe0a245b6f13b330b942f5c842afb84135
CHANGHOON <hyo040441@gmail.com>
2024-10-29 20:03:41 +0900

    [BE] Fix : add rank exception

--- server/src/main/java/artgarden/server/rank/RankController.java
+++ server/src/main/java/artgarden/server/rank/RankController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,7 +34,7 @@ public class RankController {
 
     @Operation(summary = "공연 순위 조회", description = "/ranks/20231114")
     @GetMapping("/ranks/{rankDate}")
-    public ResponseEntity<List<RankListDto>> getRank(
+    public ResponseEntity<?> getRank(
             @Parameter(description = "조회 날짜, rankDate-7 ~ rankDate 기간의 순위 조회")
             @PathVariable String rankDate,
             @Parameter(description = "랭킹 몇위까지 조회 할 것인지(최대 10, 기본 10)")
@@ -41,21 +42,27 @@ public class RankController {
         LocalDate dates = StringToLocalDate(rankDate);
         WeeklyRank weeklyRank = rankService.findByRankDate(dates);
 
-        List<String> performIds = weeklyRank.getPerformId();
+        List<String> performIds = new ArrayList<>();
         List<RankListDto> dtoList = new ArrayList<>();
+        if(weeklyRank.getPerformId() != null) {
+            performIds = weeklyRank.getPerformId();
 
-        if(size > 10){
-            size = 10;
-        }
-
-        for(int i=0; i<size; i++){     // FE요청사항 : 응답 데이터 9개로 해주세요
-            String performId = performIds.get(i);
-            if(performanceService.findById(performId) == null){
-                kopisService.saveSinglePerformance(performId);
+            if(size > 10){
+                size = 10;
             }
-            Performance performance = performanceService.findById(performId);
-            RankListDto dto = new RankListDto(performance, performIds.indexOf(performId)+1);
-            dtoList.add(dto);
+
+            for(int i=0; i<size; i++){     // FE요청사항 : 응답 데이터 9개로 해주세요
+                String performId = performIds.get(i);
+                if(performanceService.findById(performId) == null){
+                    kopisService.saveSinglePerformance(performId);
+                }
+                Performance performance = performanceService.findById(performId);
+                RankListDto dto = new RankListDto(performance, performIds.indexOf(performId)+1);
+                dtoList.add(dto);
+            }
+
+        } else {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body("No.Content");
         }
         return ResponseEntity.ok(dtoList);
     }
